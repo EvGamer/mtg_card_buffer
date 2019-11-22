5a9904ddd1d760d4d1362bd1ba28d893060f77ed
EvGamer <1evgenbetake@gmail.com>
2019-11-23 01:16:44 +0300

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const webpack = require("webpack");
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { VueLoaderPlugin } = require('vue-loader');
 
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
+  mode: "development",
   devtool: "inline-source-map",
   entry: {
     main: path.join(__dirname, "src", "popup.js"),
@@ -26,26 +26,14 @@ module.exports = {
   },
   plugins: [
     new VueLoaderPlugin(),
-    new CleanWebpackPlugin(["build"]),
-    new webpack.EnvironmentPlugin(["NODE_ENV"]),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json information
-
-        const manifest = JSON.parse(content.toString());
-
-        return Buffer.from(JSON.stringify({
-          ...manifest,
-          version: process.env.npm_package_version,
-        }))
-      }
-    }, {
-      from: "src/img/",
-      to: 'img/'
-    }, {
-      from: "src/css/",
-      to: 'css/'
-    }]),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['build/*']
+    }),
+    new CopyWebpackPlugin([
+      { from: "src/manifest.json" },
+      { from: "src/img/", to: 'img/' },
+      { from: "src/css/", to: 'css/' },
+      { from: "src/html/", to: 'html/' },
+    ]),
   ]
 };
