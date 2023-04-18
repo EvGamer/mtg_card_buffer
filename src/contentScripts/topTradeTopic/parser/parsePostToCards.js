a3e2abbf444b2963ab5ff1922002f49493ed057c
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2023-04-18 13:34:46 +0300

    Capture set name

--- src/contentScripts/topTradeTopic/parser/parsePostToCards.js
+++ src/contentScripts/topTradeTopic/parser/parsePostToCards.js
@@ -2,6 +2,7 @@ import { getLineNodes } from './getLineNodes';
 
 const RE_SEPARATOR = /,?\s/;
 const RE_PRICE = /^(\d+)[рР]?(?:уб)?/;
+const RE_SET = /^\(?((?:[a-z]\w{2})|(?:\w{2}[a-z])|(?:\w[a-z]\w))\)?/i;
 
 function convertNodesToText(childNodes, startingFrom) {
   const textContents = [];
@@ -39,9 +40,17 @@ export function parsePostToCards(postContentElement) {
     const cardInfoText = convertNodesToText(nodes, 2);
 
     cardInfoText.split(RE_SEPARATOR).forEach(word => {
+      if (card.name.trim() === 'Thorn of Amethyst') {
+        console.log(word);
+      }
       if (!card.price) {
         const priceMatch = word.match(RE_PRICE);
         if (priceMatch) card.price = Number(priceMatch[1]);
+      }
+
+      if (!card.set) {
+        const setMatch = word.trim().match(RE_SET);
+        if (setMatch) card.set = setMatch[1].toUpperCase();
       }
     })
 
